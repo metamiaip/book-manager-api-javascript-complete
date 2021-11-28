b86fc5d7baf03de451fec5ac8c5383d7a5b43794
metamiaip <metamia@gmail.com>
2021-11-28 22:16:03 +0000

    use swagger to document api in index.js file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,31 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
+// Swagger JS Doc and Swagger UI for Express
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+// Swagger Configuration
+const swaggerConfiguration = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: ' Minimalist Book Manager API',
+      version: '0.1.0',
+      description:
+        'Need a helping hand to manage your books ?' +
+        'Well, you\'re in luck! This API will let you manage your books with ease! ',
+    },
+  },
+  apis: ['src/routes/index.js'],
+  //apis: ['src\routes\index.js'],
+};
+
+// Pass the swaggerConfiguration to swaggerJSDoc
+const swaggerDocs = swaggerJSDoc(swaggerConfiguration);
+// Serve the Swagger UI at the /api-docs endpoint and setup with swaggerDocs
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.use(express.json());
 
 app.use('/api/v1', routes);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,111 @@ const express = require('express');
 const {booksController} = require('../controllers');
 
 const router = new express.Router();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Book:
+ *          type: object
+ *          required:
+ *              - title
+ *              - author
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: The auto-generated id of the book
+ *              title:
+ *                  type: string
+ *                  description: The book title
+ *              author:
+ *                  type: string
+ *                  description: The book author
+*/
+
+/**
+ * @swagger
+ * /api/v1/books:
+ *  get:
+ *      summary: Get all books
+ *      responses:
+ *          200:
+ *              description: An array of books 
+*/
 router.get('/books', booksController.getBooks);
+
+/**
+ * @swagger
+ * /api/v1/books/{bookId}:
+ *  get:
+ *      summary: Get a single book by book id
+ *      description: Retrieve a single book.
+ *      parameters:
+ *          -   in: path
+ *              name: bookId
+ *              schema:
+ *                  type: integer
+ *              required: true
+ *              description: Numeric ID of the book to retrieve
+ *      responses:
+ *          200:
+ *              description: The book description
+ *              content:
+ *                  applications/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Book'
+ *          404:
+ *              description: Not found
+*/
 router.get('/books/:bookId', booksController.getBook);
+
+/**
+ * @swagger
+ * /api/v1/books:
+ *  post:
+ *      summary: Create a book
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Book'
+ *      responses:
+ *          201:
+ *              description: book created
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Book'
+*/
 router.post('/books', booksController.saveBook);
 
+/**
+ * @swagger
+ * /api/v1/books/{bookId}:
+ *  put:
+ *      summary: update a book by id
+ *      parameters:
+ *          -   in: path
+ *              name: bookId
+ *              schema:
+ *                  type: integer
+ *              required: true
+ *              description: The book id
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Book'
+ *      responses:
+ *          204:
+ *              description: book created
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Book'
+*/
 // User Story 4 - Update Book By Id Solution
 router.put('/books/:bookId', booksController.updateBook);
 
